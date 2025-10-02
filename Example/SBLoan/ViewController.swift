b1ba9b388dac4aea3ded444c10d8252059cac6e4
Maria Clara <mariaclara.dg25@gmail.com>
2025-10-02 10:05:09 -0300

    Initial commit

---
+++ Example/SBLoan/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SBLoan
+//
+//  Created by 205457659 on 10/02/2025.
+//  Copyright (c) 2025 205457659. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
